e6467dd21f6c30857dcd0e5e07ef91dc41627bb9
RomanGorkunenko <35932569+RomanGorkunenko@users.noreply.github.com>
2018-04-02 19:57:58 +0300

    Update Users.php

--- app/Users.php
+++ app/Users.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     protected $table='users';
+    public function users() {
+    return $this->belongsTo('App\Users', 'userId');
+}
 }
